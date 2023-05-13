7edc917db5a815001c585643231e35a9981b0c95
Ahmed Kandeel <1kandeel1@gmail.com>
2023-05-13 14:46:45 +0300

    DONE

--- SchoolQuizII/SchoolQuizII.cs
+++ SchoolQuizII/SchoolQuizII.cs
@@ -20,41 +20,34 @@ namespace Problem
         /// <param name="N">number given by the teacher</param>
         /// <param name="numbers">list of possible numbers given by the teacher (starting by 1)</param>
         /// <returns>minimum number of integers whose sum equals to ‘N’</returns>
+        static public int final_sol = int.MaxValue;
+        static public KeyValuePair<int, int>[] sol_ints;
         static public int SolveValue(int N, int[] numbers)
         {
+            sol_ints = new KeyValuePair<int, int>[N + 1];
             Array.Sort(numbers);
-            //List<int> sol = new List<int>();
-            int min = int.MaxValue;
-            for (int i = numbers.Length - 1; i >= 0; i--)
+            int[] sol = new int[N + 1];
+            foreach (int i in numbers) if (i <= N) sol[i] = 1;
+            for (int i = 1; i <= N; i++)
             {
-                int n = N;
-                int j = i;
-                int cnt = 0, rem = 0;
-                //List<int> intgs = new List<int>();
-                while (true)
+                if (sol[i] == 1) continue;
+                sol[i] = int.MaxValue;
+                int a = 1, b = i - 1;
+                foreach (int intg in numbers)
                 {
-                    if (n >= numbers[j])
+                    if (intg > i) break;
+                    a = intg;
+                    b = i - intg;
+                    int tmp = sol[a] + sol[b];
+                    if (tmp < sol[i])
                     {
-                        int add = n / numbers[j];
-                        cnt += add;
-                        //while (add > 0)
-                        //{
-                        //    intgs.Add(numbers[j]);
-                        //    add--;
-                        //}
-                        rem = n % numbers[j];
-                        if (rem == 0) break;
-                        n = rem;
+                        sol[i] = tmp;
+                        sol_ints[i] = new KeyValuePair<int, int>(a, b);
                     }
-                    j--;
-                }
-                if (min > cnt)
-                {
-                    min = cnt;
-                    //sol = intgs;
                 }
             }
-            return min;
+            final_sol = sol[N];
+            return final_sol;
         }
         #endregion
 
@@ -64,39 +57,18 @@ namespace Problem
         /// <returns>the numbers themselves whose sum equals to ‘N’</returns>
         static public int[] ConstructSolution(int N, int[] numbers)
         {
-            Array.Sort(numbers);
-            List<int> sol = new List<int>();
-            int min = int.MaxValue;
-            for (int i = numbers.Length - 1; i >= 0; i--)
+            int[] sol_arr = new int[final_sol];
+
+            int i = 0;
+            int n = N;
+            while (sol_ints[n].Key != 0)
             {
-                int n = N;
-                int j = i;
-                int cnt = 0, rem = 0;
-                List<int> intgs = new List<int>();
-                while (true)
-                {
-                    if (n >= numbers[j])
-                    {
-                        int add = n / numbers[j];
-                        cnt += add;
-                        while (add > 0)
-                        {
-                            intgs.Add(numbers[j]);
-                            add--;
-                        }
-                        rem = n % numbers[j];
-                        if (rem == 0) break;
-                        n = rem;
-                    }
-                    j--;
-                }
-                if (min > cnt)
-                {
-                    min = cnt;
-                    sol = intgs;
-                }
+                sol_arr[i] = sol_ints[n].Key;
+                n = sol_ints[n].Value;
+                i++;
             }
-            return sol.ToArray();
+            sol_arr[i] = n;
+            return sol_arr;
         }
         #endregion
 
